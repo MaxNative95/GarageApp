4599cbc49345641ce1a12b91c96670a6052064fb
Max Garcia <maxgarcia@Maxs-MacBook-Pro.local>
2021-01-18 23:41:42 -0600

    feat: Changes in backend files

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,12 +2,16 @@ import 'dotenv/config'
 import cors from 'cors'
 import express from 'express'
 import router from './routes'
+const path = require('path');
 
 const app = express()
-
+// var dir = path.join(__dirname + '../assets');
+// app.use(express.static(dir));
+app.use("/assets", express.static("assets"));
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(router)
 
 export default app
+ 
